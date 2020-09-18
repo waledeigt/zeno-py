390c554d72d751fb4700b498544e5067b7d2f5e0
Dale Weigt <42933878+waledeigt@users.noreply.github.com>
2020-09-18 17:21:19 +0100

    go_chandra_analysis_tools_v2
    - Update to fix bugs with code.
    - Code streamlined

--- go_chandra_analysis_tools.py
+++ go_chandra_analysis_tools.py
@@ -6,36 +6,15 @@
 """All the relevant packages are imported for code below"""
 
 import numpy as np
-import math
-import pandas as pd
 import scipy
-from astropy.io import ascii
-from astropy.time import Time
-from astropy.visualization import astropy_mpl_style
 import matplotlib
 from matplotlib import pyplot as plt
-from matplotlib.widgets import RectangleSelector
-from matplotlib import ticker, cm
 from matplotlib import colors
-from numpy.random import poisson 
-import time
-from distutils.version import LooseVersion
 import datetime
 from datetime import datetime, timedelta
-import calendar
-import aplpy as apl
 
-import glob
-import os
-from astropy.io import fits as pyfits
-from astropy.utils.data import get_pkg_data_filename
-from astropy.table import Table, Column
-from astropy import units as u
-from astropy.units import imperial
-from astropy.convolution import Gaussian1DKernel
-from astropy.convolution import convolve, Box1DKernel
-from scipy import interpolate
-from scipy.io import readsav
+
+
 
 """Setup the font used for plotting"""
 
@@ -138,115 +117,8 @@ def select_region(x1,x2, y1,y2, bigxarr, bigyarr, bigchannel, cha_min, cha_max):
     return indx
     
     
-# def region_horizon_eph(eph_location):
-    
-#     eph_data = [] # empty array created in order to add the empheris data once loop is completed
-#     with open(eph_location) as input_data:
-#     # chandra file is read in...
-#         for line in input_data:
-#             if line.strip() == '$$SOE': #...and the Start of Empheris line is found - data beyond this line is read until...
-#                 break
-#         for line in input_data:
-#             if line.strip() == '$$EOE': #...the End of Empheris line is found.
-#                 break
-#             line = line.strip()
-#             col = line.split() # the data is then edited into columns with the new line (/n) marker removed.
-#             eph_df_data = {"date":col[0], "time":col[1], \
-#                  "RA":col[2], "DEC":col[3], "Jup_Ang-diam":col[4], "Jup_sublon":col[5], "Jup_sublat":col[6], \
-#                            "Jup_sunlon":col[7], "Jup_sunlat":col[8], "Jup_NP-ang":col[9], \
-#                            "Jup_NP-ang_dist":col[10], "Jupiter-Earth Distance (AU)":col[11], "Range_rate (deldot)": col[12]} 
-#             # dictionary of the data is created with the relevent column names - to give the data some contect when viewed - and...
-#             eph_data.append(eph_df_data) #...is added to the previosuly defined empty array to produce the data needed to create
-#             # a data frame with empheris data.
-
-#     eph_df = pd.DataFrame(eph_data) # data is inputted into a data frame...
-#     eph_dates = pd.to_datetime(eph_df['date']) #...and the time values are created into a time stamp ->  to ensure maniuplation
-#     #with time can be carried out via pandas. 
-#     eph_dates = pd.DatetimeIndex(eph_dates) # Dates of the form YYYY-MM-DD hh:mm:ss are generated from the time stamp.
-#     eph_times = pd.to_datetime(eph_df['time'])
-#     eph_times = pd.DatetimeIndex(eph_times) # The same process is carried out for the times of the emphermis file
-#     eph_doy = np.array(eph_dates.strftime('%j')) # doy from the dates are determined...
-#     eph_doy = np.array([float(i) for i in eph_doy]) #...and generated as floats to allowing manipulation of the numbrs
-
-#     eph_df['hour'] = eph_times.hour # extracts the hour from the emphermis data frame of each time
-#     eph_df['minutes'] = eph_times.minute # extracts the minute from the emphermis data frame of each time
-#     eph_df['seconds'] = eph_times.second # extracts the second from the emphermis data frame of each time
-#     eph_df[['hour', 'minutes', 'seconds']] = eph_df[['hour', 'minutes', 'seconds']].astype(float) # changes the selected columns to 
-#     # floats
-#     eph_df['DOY'] = eph_doy # creates a new column - DOY (from the doy values calculated)
-#     #eph_df['DOYFRAC'] = doy_frac(eph_doy, eph_df['hour'], eph_df['minutes'], eph_df['seconds']) # DOYFRAC is calculated for the dates
-#     # and added as a new column in data frame
-#     #eph_doyfrac = np.array(eph_df['DOYFRAC'])
-#     eph_month = np.array(eph_dates.month) # changes the month data into an array
-#     eph_day = np.array(eph_dates.day)
-
-#     bday = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334] # the last DOY associated with each month...
-#     bday = np.array([float(i) for i in bday]) #...and converted into floats
-
-#     eph_leap = eph_dates.is_leap_year # To check if the date is a leap year
-#     bdayx = bday[eph_month - 1] # end of doy associated with the (month number -1) from the horizons file
-#     leap_condition = np.where((eph_leap == True) & (eph_month > 2)) # look for non-zero values where the date is a leap year and the 
-#     # month of observation is beyond Feb...
-#     #leap_condition = np.where((eph_leap_test == True) & (eph_month_test > 2))
-#     leap_count = np.count_nonzero(leap_condition)#...and then count the number of times this occurs
-
-#     if leap_count > 0:                                        # if there are more than zero instances where the condition is true...
-#         print('Is a leap year - applying correction to DOY.') #... the date is a leap year and...
-#         bdayx[leap_condition] = bdayx[leap_condition] + 1     #...add 1 day to the end of month when this condition holds and...
-#         eph_doyfrac = doy_frac(bdayx, eph_df['hour'], eph_df['minutes'], eph_df['seconds']) + eph_day
-#         #...the DOYFRAC is calculated for the dates (leap year) and...
-#         eph_df['DOYFRAC'] = eph_doyfrac #...added as a new column in the data frame
-#     else:                                                     
-#         #print('Not a leap year')                              # if the date is not a leap year, carry out the same calculation
-#                                                           # without correcting for a leap year
-#         eph_doyfrac = doy_frac(bdayx, eph_df['hour'], eph_df['minutes'], eph_df['seconds']) + eph_day
-#         eph_df['DOYFRAC'] = eph_doyfrac 
-
-#     return eph_df
-    
-
-# def region_hist(scale, xt, yt, limb, xevents, yevents, tevents, obs_id):
-#     #data = ascii.read("%s_selected_region.txt" % obs_id)
-#     #print(data)
-#     fig, axes=plt.subplots(figsize=(8,7))
-#     axes = plt.gca()
-#     axes.grid(False)
-#     #plt.style.use(astropy_mpl_style)
-#     plt.plot(xt[limb]*scale, yt[limb]*scale, color='cyan',alpha=0.3)
-#     reg_plot = plt.plot(xevents,yevents, 'o', markersize=0.5,linestyle='None',color='blue')
-#     axes.set_title('Selected Events (ObsID %s)' % obs_id)
-#     axes.set_xlabel('X (arcsecs)')
-#     axes.set_ylabel('Y (arcsecs)')
-#     axes.set_ylim(-40,40)
-#     axes.set_xlim(-50,50)
-#     bigtime_reg = tevents
-#     beg, end, offset = selecttime(bigtime_reg)
-#     #print('This is "beg" = %f' % beg)
-#     #print('This is "end" = %f' % end)
-#     #print(offset)
-#     binsizewidth = 240
-#     #hist, bin_edges = np.histogram(offset)
-#     #arr = np.arange(len(hist))*binsizewidth
-
-#     fig, axes=plt.subplots(figsize = (8,7))
-#     axes = plt.gca()
-#     axes.grid(False)
-#    #plt.style.use(astropy_mpl_style)
-
-#     hist_plot = plt.hist(offset, bins=np.arange(min(offset), max(offset) + binsizewidth, binsizewidth), \
-#                          histtype = 'step', fill = None)
-#     axes.set_xlabel('Offset from %e secs' % bigtime_reg[0])
-#     axes.set_ylabel('Events per %i secs' % binsizewidth)
-#     axes.set_title('Histogram of Event Times for Selected Region (ObsID %s)' % obs_id)
-    
-#     print('max(xt[limb]) = %f' % max(xt[limb]))
-#     print('max(yt[limb]) = %f' % max(yt[limb]))
-    
-#     return reg_plot, hist_plot, bigtime_reg
-
-
-def plotprops(props,st,units,sup_lon_list,sup_lat_list,obs_id=obs_id, custom_map):
-    
+def plotprops(props,st,units,sup_lon_list,sup_lat_list,obs_id, custom_map):
+          
     """Plots the X-ray emission in given units across Jupiter's surface on a S3 longitude vs. latitude grid. The Io and magnetotail footprints are also plotted. The inputs are defined as follows:
     
     props:   the property of the X-rays plotted onto the map. For example, Chandra code provides a flux, exposure time and a brightness (using a conversion factor).
@@ -329,9 +201,9 @@ def plotprops(props,st,units,sup_lon_list,sup_lat_list,obs_id=obs_id, custom_map
     plt.close()
     return 
 
-def plotprops_region_select(props,st,units,sup_lon_list,sup_lat_list,obs_id=obs_id, custom_map):
+def plotprops_region_select(props,st,units,sup_lon_list,sup_lat_list,obs_id, custom_map):
     
-        """Plots the X-ray emission in given units across Jupiter's surface on a S3 longitude vs. latitude grid. The code prompts the user to select the range of S3 longitudes and latitudes to map. The Io and magnetotail footprints are also plotted. The inputs are defined as follows:
+    """Plots the X-ray emission in given units across Jupiter's surface on a S3 longitude vs. latitude grid. The code prompts the user to select the range of S3 longitudes and latitudes to map. The Io and magnetotail footprints are also plotted. The inputs are defined as follows:
     
     props:   the property of the X-rays plotted onto the map. For example, Chandra code provides a flux, exposure time and a brightness (using a conversion factor).
     
@@ -346,7 +218,6 @@ def plotprops_region_select(props,st,units,sup_lon_list,sup_lat_list,obs_id=obs_
     obs_id:  the observation id - this is set as default
     
     custom_map:     custom color map produced from the c_map script."""
-        
         
     geomsy_data = scipy.io.readsav('geomsy.sav')
     lam_vip = geomsy_data['lam_vip'] 
@@ -367,10 +238,7 @@ def plotprops_region_select(props,st,units,sup_lon_list,sup_lat_list,obs_id=obs_
     axes.plot([0,359], [-90,90],linestyle='None')
     axes.set_title(st)
     axes.text((x_lim_1 + x_lim_2)/2, y_lim_1-5, 'Max = %f %s' % (np.amax(props), units), fontsize=15, horizontalalignment='center')
-#    scale = 255/np.amax(bins)
-#    z = np.arange(181)
-#    a = np.zeros(181)
-    #print(a+359)
+
     lngarr = np.arange(360)
     
     max_cbar = np.amax(props)
@@ -400,11 +268,11 @@ def plotprops_region_select(props,st,units,sup_lon_list,sup_lat_list,obs_id=obs_
     for i in range(0,2):
         lng = lam306_vip[i,:]
         lat = r306thet_vip[i,:]
-#        print(lng)
+
         dl = lng[1:36] - lng[0:35]
-#        print(dl)
+
         p_cond = np.where(abs(dl) > 180)[0]
-#        print(p_cond)
+
         lat = np.roll(lat, -p_cond[0]-1)
         lng = np.roll(lng, -p_cond[0]-1)
         axes.plot(lng,lat, color='white')
@@ -426,22 +294,7 @@ def plotprops_region_select(props,st,units,sup_lon_list,sup_lat_list,obs_id=obs_
     #plt.close()
     return x_lim_1, x_lim_2, y_lim_1, y_lim_2
 
-def plotprops_time(props,st,units,sup_lon_list,sup_lat_list,obs_id=obs_id):
-    
-        """Plots the exposure time of Chandra in seconds across Jupiter's surface on a S3 longitude vs. latitude grid. The Io and magnetotail footprints are also plotted. The inputs are defined as follows:
-    
-    props:   the property of the X-rays plotted onto the map. For example, Chandra code provides a flux, exposure time and a brightness (using a conversion factor).
-    
-    st:     string for the title of the plot.
-    
-    units:  units of the color bar used in the map.
-    
-    sup_lon_list:   the list of longitudes for the X-ray emission found.
-    
-    sup_lat_list:   the list of latitudes for the X-ray emission found.
-    
-    obs_id:  the observation id - this is set as default"""
-        
+def plotprops_time(props,st,units,sup_lon_list,sup_lat_list,obs_id):
     geomsy_data = scipy.io.readsav('geomsy.sav')
     lam_vip = geomsy_data['lam_vip'] 
     rthet_vip = geomsy_data['rthet_vip'] 
@@ -457,10 +310,7 @@ def plotprops_time(props,st,units,sup_lon_list,sup_lat_list,obs_id=obs_id):
     axes.plot([0,359], [-90,90],linestyle='None')
     axes.set_title(st)
     axes.text(175, -115, 'Max = %f %s' % (np.amax(props), units), fontsize=15, horizontalalignment='center')
-#    scale = 255/np.amax(bins)
-#    z = np.arange(181)
-#    a = np.zeros(181)
-    #print(a+359)
+
     lngarr = np.arange(360)
     
     max_cbar = np.amax(props)
@@ -484,11 +334,11 @@ def plotprops_time(props,st,units,sup_lon_list,sup_lat_list,obs_id=obs_id):
     for i in range(0,2):
         lng = lam306_vip[i,:]
         lat = r306thet_vip[i,:]
-#        print(lng)
+
         dl = lng[1:36] - lng[0:35]
-#        print(dl)
+
         p_cond = np.where(abs(dl) > 180)[0]
-#        print(p_cond)
+
         lat = np.roll(lat, -p_cond[0]-1)
         lng = np.roll(lng, -p_cond[0]-1)
         axes.plot(lng,lat, color='white')
@@ -510,10 +360,9 @@ def plotprops_time(props,st,units,sup_lon_list,sup_lat_list,obs_id=obs_id):
     plt.close()
     return
 
-def plotprops_int(iteration,props,st,units,sup_lon_list,sup_lat_list,obs_id,folder=folder, custom_map):
+def plotprops_int(iteration,props,st,units,sup_lon_list,sup_lat_list,obs_id, custom_map, folder):
     
-        
-        """Plots the X-ray emission in given units across Jupiter's surface on a S3 longitude vs. latitude grid. Multiple maps are produced depending on the number of intervals the obsrvation has baan split into. The Io and magnetotail footprints are also plotted. The inputs are defined as follows:
+    """Plots the X-ray emission in given units across Jupiter's surface on a S3 longitude vs. latitude grid. Multiple maps are produced depending on the number of intervals the obsrvation has baan split into. The Io and magnetotail footprints are also plotted. The inputs are defined as follows:
     
     props:   the property of the X-rays plotted onto the map. For example, Chandra code provides a flux, exposure time and a brightness (using a conversion factor).
     
@@ -531,7 +380,8 @@ def plotprops_int(iteration,props,st,units,sup_lon_list,sup_lat_list,obs_id,fold
     
     custom_map:     custom color map produced from the c_map script.
     
-    folder:   the file path for the saved plots"""
+    folder:   the file path for the saved plots
+    """
         
     geomsy_data = scipy.io.readsav('geomsy.sav')
     lam_vip = geomsy_data['lam_vip'] 
@@ -548,10 +398,7 @@ def plotprops_int(iteration,props,st,units,sup_lon_list,sup_lat_list,obs_id,fold
     axes.plot([0,359], [-90,90],linestyle='None')
     axes.set_title(st)
     axes.text(175, -115, 'Max = %f %s' % (props.max(), units), fontsize=15, horizontalalignment='center')
-#    scale = 255/np.amax(bins)
-#    z = np.arange(181)
-#    a = np.zeros(181)
-    #print(a+359)
+
     lngarr = np.arange(360)
     
     max_cbar = np.amax(props)
@@ -575,11 +422,11 @@ def plotprops_int(iteration,props,st,units,sup_lon_list,sup_lat_list,obs_id,fold
     for i in range(0,2):
         lng = lam306_vip[i,:]
         lat = r306thet_vip[i,:]
-#        print(lng)
+
         dl = lng[1:36] - lng[0:35]
-#        print(dl)
+
         p_cond = np.where(abs(dl) > 180)[0]
-#        print(p_cond)
+
         lat = np.roll(lat, -p_cond[0]-1)
         lng = np.roll(lng, -p_cond[0]-1)
         axes.plot(lng,lat, color='white')
@@ -602,9 +449,9 @@ def plotprops_int(iteration,props,st,units,sup_lon_list,sup_lat_list,obs_id,fold
     #plt.savefig(r'%s_map%03i.png' % (obs_id,iteration))
     return
 
-def plotprops_int_time(iteration,props,st,units,sup_lon_list,sup_lat_list,obs_id,folder=folder):
+def plotprops_int_time(iteration,props,st,units,sup_lon_list,sup_lat_list,obs_id,folder):
     
-          """Plots the exposure time of Chandra in seconds across Jupiter's surface on a S3 longitude vs. latitude grid. Multiple timemaps are produced depending on the number of intervals the obsrvation has baan split into. The Io and magnetotail footprints are also plotted. The inputs are defined as follows:
+    """Plots the exposure time of Chandra in seconds across Jupiter's surface on a S3 longitude vs. latitude grid. Multiple timemaps are produced depending on the number of intervals the obsrvation has baan split into. The Io and magnetotail footprints are also plotted. The inputs are defined as follows:
     
     iteration:      the counter used to map all time interavls of the Chandra observation.
     
@@ -637,10 +484,7 @@ def plotprops_int_time(iteration,props,st,units,sup_lon_list,sup_lat_list,obs_id
     axes.plot([0,359], [-90,90],linestyle='None')
     axes.set_title(st)
     axes.text(175, -115, 'Max = %f %s' % (np.amax(props), units), fontsize=15, horizontalalignment='center')
-#    scale = 255/np.amax(bins)
-#    z = np.arange(181)
-#    a = np.zeros(181)
-    #print(a+359)
+
     lngarr = np.arange(360)
 
     
@@ -655,11 +499,11 @@ def plotprops_int_time(iteration,props,st,units,sup_lon_list,sup_lat_list,obs_id
     for i in range(0,2):
         lng = lam306_vip[i,:]
         lat = r306thet_vip[i,:]
-#        print(lng)
+
         dl = lng[1:36] - lng[0:35]
-#        print(dl)
+
         p_cond = np.where(abs(dl) > 180)[0]
-#        print(p_cond)
+
         lat = np.roll(lat, -p_cond[0]-1)
         lng = np.roll(lng, -p_cond[0]-1)
         axes.plot(lng,lat, color='white')
